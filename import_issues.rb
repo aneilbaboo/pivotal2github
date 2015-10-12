e00cd5e0b0631be7f879f7afb3e8b0062162df3a
Aneil Mallavarapu <aneil.mallavar@gmail.com>
2015-10-12 09:38:08 -0700

    Improve output

--- import_issues.rb
+++ import_issues.rb
@@ -31,35 +31,36 @@ CSV.open filename, :headers => true do |csv|
       :title => r['Title'],
       :body => r['Description'] || "",
     }
+    puts "-----------------------------------------------\nADDING ISSUE: #{body[:title]}\nDESCRIPTION: '#{body[:body]}'"
     labels = []
 
     if r['Labels'] != ''
       r['Labels'].split(',').each do |label|
         label = label.strip
-        color ='' 
-        3.times { 
+        color =''
+        3.times {
           color << "%02x" % rand(255)
         }
        unless visited_labels.include? label
-        labels << {:name => label, :color =>color} 
+        labels << {:name => label, :color =>color}
        end
       end
       labels.each do |label|
-        p label
+        puts label
         # this hack doesn't care if you have an existing label - it just errors and moves on like a zen master
         # the server however is expected to be equally zen :D
         visited_labels << label[:name]
         label = GitHub.post "/repos/#{repository_path}/labels", :body => JSON.generate(label)
-        p label
+        puts label
       end
     end
-    
+
     body[:labels] = r['Labels'].split(',').map {|l|l.strip} if r['Labels'] != ''
 
-   
-    p json_body = JSON.generate(body)
+
+    json_body = JSON.generate(body)
     issue = GitHub.post "/repos/#{repository_path}/issues", :body => json_body
-    p issue
+    puts issue
 
     r.each do |f|
       if f[0] == 'Comment'
